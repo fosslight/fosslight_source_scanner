b96e41bfee8623921c1adc024f00c2a5a9d0696e
Soim Kim <soim.kim@lge.com>
2020-10-26 16:28:08 +0900

    Change package name to fosslight_source

--- README.md
+++ README.md
@@ -3,17 +3,17 @@
 $ pip install virtualenv     
 $ virtualenv -p /usr/bin/python3.6 venv     
 $ source venv/bin/activate        
-$ pip install http://mod.lge.com/code/rest/archive/latest/projects/OSC/repos/scancode_to_excel/archive?format=zip        
+$ pip install http://mod.lge.com/code/rest/archive/latest/projects/OSC/repos/fosslight_source/archive?format=zip             
 
 <hr />       
 
 ## How to run it by command
 ### Print result to OSS Report
-$ run_scancode -p [Path_to_scan]
+$ fosslight_source -p [Path_to_scan]
 ### Print result to OSS Report and json file
-$ run_scancode -p [Path_to_scan] -j
+$ fosslight_source -p [Path_to_scan] -j
 ## How to run the script - Converting scancode json result to OSS report
-$ convert_scancode -p [Path_of_scancode_json_files]
+$ fosslight_convert -p [Path_of_scancode_json_files]
 
 <hr />     
 
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.md', 'r', 'utf-8') as f:
 
 if __name__ == "__main__":
     setup(
-        name             = 'scancode_to_excel',
+        name             = 'fosslight_source',
         version          = '1.0',
         packages         = find_packages(),
         description      = 'Scancode analysis in OSS Report format',
@@ -19,8 +19,8 @@ if __name__ == "__main__":
         license          = 'LGE-Proprietary',
         author           = 'Soim Kim',
         author_email     = 'soim.kim@lge.com',
-        url              = 'http://mod.lge.com/code/projects/OSC/repos/scancode_to_excel',
-        download_url     = 'http://mod.lge.com/code/rest/archive/latest/projects/OSC/repos/scancode_to_excel/archive?format=zip',
+        url              = 'http://mod.lge.com/code/projects/OSC/repos/fosslight_source',
+        download_url     = 'http://mod.lge.com/code/rest/archive/latest/projects/OSC/repos/fosslight_source/archive?format=zip',
         classifiers      = ['Programming Language :: Python :: 3.6',
                         'License :: OSI Approved :: Closed Sorce Software'],
         install_requires = [
@@ -29,8 +29,10 @@ if __name__ == "__main__":
         ],
         entry_points = {
             "console_scripts": [
-                "convert_scancode = scancode_to_excel.convert_scancode:main",
-                "run_scancode = scancode_to_excel.run_scancode:main"
+                "fosslight_convert = fosslight_source.convert_scancode:main",
+                "fosslight_source = fosslight_source.run_scancode:main",
+                "convert_scancode = fosslight_source.convert_scancode:main",
+                "run_scancode = fosslight_source.run_scancode:main"
                 ]
             }
     )
