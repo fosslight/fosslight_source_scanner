14c770310d2f4201c1814ef5cc7d11f613c90879
Wonjae Park <wonjae.park@lge.com>
2022-12-28 13:44:52 +0900

    Remove warning for -m option with default output

--- src/fosslight_source/cli.py
+++ src/fosslight_source/cli.py
@@ -94,7 +94,7 @@ def main():
 
     _start_time = datetime.now().strftime('%y%m%d_%H%M')
     success, msg, output_path, output_file, output_extension = check_output_format(output_file_name, format)
-    if output_extension != '.xlsx' and print_matched_text:
+    if output_extension != '.xlsx' and output_extension != "" and print_matched_text:
         logger.warning("-m option is only available for excel.")
         print_matched_text = False
     if not success:
