df66df2a0f9d3385aadf447daf9480b1e443105a
Jaekwon Bang <jaekwon.bang@lge.com>
2023-02-27 09:57:21 +0900

    Set version 1.6.24 in setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('requirements.txt', 'r', 'utf-8') as f:
 if __name__ == "__main__":
     setup(
         name='fosslight_source',
-        version='1.6.25',
+        version='1.6.24',
         package_dir={"": "src"},
         packages=find_packages(where='src'),
         description='FOSSLight Source Scanner',
