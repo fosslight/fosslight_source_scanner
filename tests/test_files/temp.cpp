318ecadb2377abb6e3848667449ee8e0782fc070
Soim <soim.kim@lge.com>
2023-04-12 17:10:00 +0900

    Add test files

---
+++ tests/test_files/temp.cpp
@@ -0,0 +1,12 @@
+/*
+  * SPDX-FileCopyrightText: Copyright 2022 LG Electronics Inc.
+  * SPDX-License-Identifier: Apache-2.0
+  * DownloadLocation: https://github.com/browserify/acorn-node
+ */
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
+}
