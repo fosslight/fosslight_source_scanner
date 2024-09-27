75d3b19b1e859822b71cbc338723783daf9aa20b
hkkim <khk990619@gmail.com>
2024-09-27 22:36:21 +0900

    test_tox.py 추가
    Signed-off-by: hkkim <khk990619@gmail.com>

---
+++ tests/test_tox.py
@@ -0,0 +1,69 @@
+import os
+import subprocess
+import pytest
+
+def run_command(command):
+    process=subprocess.run(command, shell=True, capture_output=True, text=True)
+    success= (process.returncode ==0)
+    return success, process.stdout if success else process.stderr
+
+@pytest.fixture(scope="module", autouse=True)
+def setup_and_cleanup():
+    run_command("rm -rf test_scan test_scan2 test_scan3")
+    os.makedirs("test_scan")
+
+def test_run():
+    scan_success,_ = run_command("fosslight_source -p tests/test_files -j -m -o test_scan")
+    scan_exclude_success,_=run_command("fosslight_source -p tests -e test_files/test cli_test.py -j -m -o test_scan2")
+
+    scan_files=os.listdir("test_scan")
+    scan2_files=os.listdir('test_scan2')
+
+    #then
+    assert scan_success is True, "Test Run: Scan command failed"
+    assert scan_exclude_success is True, "Test Run: Exclude command failed"
+    
+    assert len(scan_files)>0, "Test Run: No scan files created in test_scan directory"
+    assert len(scan2_files)>0, "Test Run: No scan files created in test_scan2 directory"
+
+def test_help_command():
+    success, _ = run_command("fosslight_source -h")
+    assert success is True, "Test Release: Help command failed "
+
+def test_scan_command():
+    success,_= run_command("fosslight_source -p tests/test_files -o test_scan/scan_result.csv")
+    assert success is True, "Test Release: Failed to generate scan result CSV file"
+
+    assert os.path.exists("test_scan/scan_result.csv"), "Test Release: scan_result.csv file not generated"
+
+    with open("test_scan/scan_result.csv", 'r') as file:
+        content = file.read()
+
+    assert len(content) > 0, "Test Release: scan_result.csv is empty"
+    print(f"Content of scan_result.csv:\n{content}")
+
+def test_exclude_command():
+    success,_= run_command("fosslight_source -p tests -e test_files/test cli_test.py -j -m -o test_scan2/scan_exclude_result.csv")
+    assert success is True, "Test release: Exclude scan failded"
+
+    assert os.path.exists("test_scan2/scan_exclude_result.csv"), "Test Release: scan_exclude_result.csv file not generated"
+
+    with open("test_scan2/scan_exclude_result.csv", 'r') as file:
+        content = file.read()
+
+    assert len(content) > 0, "Test Release: scan_exclude_result.csv is empty"
+    print(f"Content of scan_exclude_result.csv:\n{content}")
+
+def test_json_command():
+    success,_=run_command("fosslight_source -p tests/test_files -m -j -o test_scan3/")   
+    assert success is True, "Test release: Failed to generate JSON files"
+
+def test_ls_test_scan3_command():
+    files_in_test_scan3 = os.listdir("test_scan3")
+    assert len(files_in_test_scan3) > 0, "Test Release: test_scan3 is empty"
+    print(f"Files in test_scan3: {files_in_test_scan3}")
+
+def test_flake8():
+    success,_ =run_command("flake8")
+    assert success is True, "Flake8: Style check failed"
+
