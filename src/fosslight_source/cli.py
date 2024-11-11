6263bc281a8d1a7a516db245939b48e50e3e3864
Ethan Lee <seojethan.lee@lge.com>
2024-11-11 11:30:08 +0000

    Test rollback cli.py
    Signed-off-by: Ethan Lee <seojethan.lee@lge.com>

--- src/fosslight_source/cli.py
+++ src/fosslight_source/cli.py
@@ -205,10 +205,7 @@ def create_report_file(
     scan_item.set_cover_pathinfo(path_to_scan, path_to_exclude)
     files_count, removed_files_count = count_files(path_to_scan, path_to_exclude)        
     scan_item.set_cover_comment(f"Total number of files / removed files: {files_count} / {removed_files_count}")
-    
-    #scan_item.set_cover_comment(f"Total number of files : {files_count}")
-    #scan_item.set_cover_comment(f"Removed files : {removed_files_count}")
-    
+     
     if not merged_result:
         if files_count < 1:
             scan_item.set_cover_comment("(No file detected.)")
